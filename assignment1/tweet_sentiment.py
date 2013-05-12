a59e2132b343615b79d674f2d37d6bb49013afa9
Max Gribov <max@neuropunks.org>
2013-05-12 19:04:11 -0400

    sentiment score

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,25 +1,39 @@
 import sys
 import json
+import re
 
-def hw(text, lookup):
-    print text
-
-def lines(fp):
-    print str(len(fp.readlines()))
+def hw(text, lookup, regex):
+    sent = 0
+    words = re.sub(regex, ' ', text).split(' ')
+    #words = text.split(' ')
+    
+    for word in words:
+        try:
+            sent = sent + lookup[word]
+        except KeyError:
+            continue
+    return float(sent)    
 
 def main():
+    pattern = re.compile('\W')
+
     sent_file = open(sys.argv[1])
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+      term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+      scores[term] = int(score)  # Convert the score to an integer.
+
+    #print scores.items() # Print every (term, score) pair in the dictionary
+
     tweet_file = open(sys.argv[2])
 
     for line in tweet_file:
         t = json.loads(line)
         try:
-            hw(t['text'])
+            #print t['text']
+            print hw(t['text'], scores, pattern)
         except KeyError:
             continue
-
-    #lines(sent_file)
-    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
