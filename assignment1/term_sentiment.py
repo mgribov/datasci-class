7bd00e1f064af4aa879b0e16166cc93f271a293e
Max Gribov <max@neuropunks.org>
2013-05-12 20:31:57 -0400

    term sentiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,60 @@
 import sys
+import json
+import re
 
-def hw():
-    print 'Hello, world!'
+def hw(text, lookup, regex):
+    new = {}
+    sent = 0
+    words = re.sub(regex, ' ', text).split(' ')
+    
+    for word in words:
+        try:
+            sent = sent + lookup[word]
+        except KeyError,ValueError:
+            
+            try:
+                new[word] = new[word] + 1
+            except KeyError,ValueError:
+                new[word] = 1
+                
+            continue
 
-def lines(fp):
-    print str(len(fp.readlines()))
+    return (float(sent), new)    
+
 
 def main():
+    sentiments = {}
+    counts = {}
+    new = {}
+
+    pattern = re.compile('\W')
+
     sent_file = open(sys.argv[1])
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+      term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+      scores[term] = int(score)  # Convert the score to an integer.
+
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    for line in tweet_file:
+        t = json.loads(line)
+        try:
+            (tweet_sent, new) = hw(t['text'], scores, pattern)
+            for n in new:
+                try:
+                    sentiments[n] = sentiments[n] + tweet_sent
+                    counts[n] = counts[n] + 1
+                except KeyError,ValueError:
+                    sentiments[n] = tweet_sent
+                    counts[n] = 1
+
+        except KeyError:
+            continue
+
+    for term in sentiments:
+        print "%s %.3f" % (term, sentiments.get(term) / counts.get(term))
+
 
 if __name__ == '__main__':
     main()
